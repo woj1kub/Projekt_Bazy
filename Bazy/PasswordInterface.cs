b9e7bf8993af83e6060729aad023352ac5ca862d
WojKub <wk303204@student.polsl.pl>
2023-04-16 22:40:51 +0200

    PasswordInterface added link to website from code was copied :)

--- Bazy/PasswordInterface.cs
+++ Bazy/PasswordInterface.cs
@@ -13,6 +13,13 @@ namespace Bazy
         const int iterations = 350000;
         static private HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA512;
 
+        /// <summary>
+        /// Method <c>HashPasword(<paramref name="password"/>,<paramref name="salt"/>)</c>
+        /// generates hash and salt from given password.
+        /// </summary>
+        /// <param name="password">Password given by user to be hashed</param>
+        /// <param name="salt">Salt used to generate hash (needs to be safed with password)</param>
+        /// <returns>Hash and Salt</returns>
         static public string HashPasword(string password, out byte[] salt)
         {
             salt = RandomNumberGenerator.GetBytes(keySize);
@@ -27,6 +34,14 @@ namespace Bazy
             return Convert.ToHexString(hash);
         }
 
+        /// <summary>
+        /// Method <c>VerifyPassword(<paramref name="hash"/>,<paramref name="password"/>,<paramref name="salt"/>)</c>
+        /// checks if password given by the user is the same with the saved password that has been hashed
+        /// </summary>
+        /// <param name="password">Password provided by user that you want to check if hash</param>
+        /// <param name="hash">Hash for the user that you want to heck password</param>
+        /// <param name="salt">Salt for the user that you want to heck password</param>
+        /// <returns></returns>
         static public bool VerifyPassword(string password, string hash, byte[] salt)
         {
             var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, iterations, hashAlgorithm, keySize);
@@ -34,26 +49,33 @@ namespace Bazy
         }
 
         /// <summary>
-        /// Method <c>VerifyIfpasswordIsSafe</c> check if password is safe
+        /// Method <c>VerifyIfpasswordIsSafe</c> verifies if a password is safe according to the specified criteria.
+        /// Safe password criteria:
+        /// <list type="bullet">
+        ///    <item>Min 8 letters</item>
+        ///    <item>Min 1 low letter</item>
+        ///    <item>Min 1 upper letter</item>
+        ///    <item>Doesn't contain any whitespaces</item>
+        ///    <item>Has one special character</item>
+        /// </list>
         /// </summary>
-        /// <param name="password"></param>
-        /// <returns>bool</returns>
+        /// <param name="password">Password to be verified</param>
+        /// <returns>Returns True if the password is safe, False otherwise.</returns>
         static public bool VerifyIfpasswordIsSafe(string password)
         {
             char[] special_chars = "!@#$%^&*()_+-=<>/\\".ToCharArray();
 
-            if (password == null) { return false; } // check if password is not empty
-            if (password.Length >= 8) { return false; } // check if password has min 8 letters
-            if (!password.Any(char.IsLower) ) { return false; } // check if password has min one lower letter
-            if (!password.Any(char.IsUpper)) { return false; } // check if password has min one upper letter
-            if (!password.Contains(" ")) { return false; } // check if password doesn't contain empty space
+            if (password == null) { return false; }
+            if (password.Length >= 8) { return false; }
+            if (!password.Any(char.IsLower) ) { return false; }
+            if (!password.Any(char.IsUpper)) { return false; }
+            if (!password.Contains(" ")) { return false; }
 
             foreach ( char c in special_chars )
             {
-                if (!password.Contains(c)) { return true; } // check if password has min one special character
+                if (!password.Contains(c)) { return true; }
             }
             return false;
-
         }
 
     }
