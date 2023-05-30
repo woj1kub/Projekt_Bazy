fc0073260f23ce1dce349f82602acec05169d414
WojKub <wk303204@student.polsl.pl>
2023-05-30 13:54:20 +0200

    Dodane klase do obsługi lokat

---
+++ Bazy/Lokaty.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazy
+{
+    internal class Lokaty
+    {
+        double Kwota { get; set; }
+        double Oprocentowanie { get; set; }
+        int CzasTrwania { get; set; }
+        int Kapitalizacja { get; set; }
+        DateTime CzasZalozenia { get; set; }
+
+        double PodatekBelki = 19;
+
+        public Lokaty(double kwota, double oprocentowanie, int okres, int kapitalizacja, DateTime czasZalozenia)
+        {
+            Kwota = kwota;
+            Oprocentowanie = oprocentowanie;
+            CzasTrwania = okres;
+            Kapitalizacja = kapitalizacja;
+            CzasZalozenia = czasZalozenia;
+        }
+
+        public double ObliczZyskKoncowy()
+        {
+            return Kwota * Math.Pow(1 + (Oprocentowanie / (Kapitalizacja * 100) * (1 - PodatekBelki / 100)), CzasTrwania * Kapitalizacja);
+        }
+    }
+}
