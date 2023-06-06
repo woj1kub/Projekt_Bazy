965ed2969373c48362d0c8af457c5d93f9084c1a
Maciej Wielgosz <92044905+MacWielgosz@users.noreply.github.com>
2023-06-06 10:22:32 +0200

    Dodanie dalszych zmian w portfelu

--- Bazy/Portfele.xaml.cs
+++ Bazy/Portfele.xaml.cs
@@ -25,8 +25,10 @@ namespace Bazy
     public partial class Portfele : UserControl
     {
         private readonly string ActiveUser;
-        struct Portfel_Gotówkowy
+        //tymczasowa struktura portfelu póki nie zostanie zrobiona klasa dla całego porjektu
+        struct Portfele_info
         {
+            public Int64 PortfeleId;
             public string nazwa;
             public decimal wartosc;
 
@@ -35,7 +37,8 @@ namespace Bazy
                 return nazwa +" " +wartosc.ToString();
             }
         }
-        ObservableCollection<Portfel_Gotówkowy> portfele_dane=new();
+        ObservableCollection<Portfele_info> portfele_dane=new();
+        ObservableCollection<String> nazwy_porteli { get; set; } = new();
         public string ActivePortfef { set; get; }=string.Empty;
         public Portfele(string ActiveUser)
         {
@@ -52,19 +55,21 @@ namespace Bazy
         {
             var conn = new NpgsqlConnection(Registration.ConnString());
             conn.Open();
-            NpgsqlCommand cmd = new NpgsqlCommand("SELECT \"Nazwa_Portfelu\",\"Kwota\" " +
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT \"Id_Portfelu\",\"Nazwa_Portfelu\",\"Kwota\" " +
                 "FROM \"Portfele\" INNER JOIN \"Portfel Gotówkowy\" ON \"Id_Porfelu\"=\"Id_Portfelu\" " +
                 "WHERE \"Użykownik\"=@login");
 
             cmd.Parameters.AddWithValue("@login", ActiveUser);
             cmd.Connection=conn;
             NpgsqlDataReader reader= cmd.ExecuteReader();
-            Portfel_Gotówkowy portfel;
+            Portfele_info portfel;
 
             while (reader.Read())
             {
-                portfel.nazwa = reader.GetString(0);
-                portfel.wartosc = reader.GetDecimal(1);
+                portfel.PortfeleId = reader.GetInt64(0);
+                portfel.nazwa = reader.GetString(1);
+                nazwy_porteli.Add(reader.GetString(1));
+                portfel.wartosc = reader.GetDecimal(2);
                 portfele_dane.Add(portfel);
             }
             conn.Close();
@@ -73,8 +78,8 @@ namespace Bazy
         void Wyswietlanie()
         {
             lbiPortfele.ItemsSource = portfele_dane;
-            cbPortfel1.ItemsSource = portfele_dane;    
-            cbPortfel2.ItemsSource = portfele_dane;    
+            cbPortfel1.ItemsSource = nazwy_porteli;    
+            cbPortfel2.ItemsSource = nazwy_porteli;    
         }
         private void btDodaj_Click(object sender, RoutedEventArgs e)
         {
@@ -93,23 +98,30 @@ namespace Bazy
             cmd.Parameters.AddWithValue("@name", NewPortfelName.Text);
             cmd.Connection=conn;
             var id_portfelea = cmd.ExecuteScalar();
+            if (id_portfelea == null)
+                return;
             //Dodanie nowego portfelu gotówkowego
             cmd = new NpgsqlCommand("INSERT INTO \"Portfel Gotówkowy\" (\"Id_Porfelu\" , \"Kwota\") " +
                 "VALUES ((SELECT \"Id_Portfelu\" FROM \"Portfele\" WHERE \"Id_Portfelu\" = @Id_portfel ) , @kwotaPortfela)");
             cmd.Parameters.AddWithValue("@kwotaPortfela", decimal.Parse(Fundusze.Text) );
             cmd.Parameters.AddWithValue("Id_portfel", id_portfelea);
-
             cmd.Connection = conn;
             cmd.ExecuteNonQuery();
             conn.Close();
-            Portfel_Gotówkowy portfel;
+            //dodanie nowego portfelu do listy portfeli
+            Portfele_info portfel;
+            portfel.PortfeleId =(Int64) id_portfelea;
             portfel.nazwa = NewPortfelName.Text;
             portfel.wartosc =decimal.Parse( Fundusze.Text);
             portfele_dane.Add(portfel);
-            
+            nazwy_porteli.Add(NewPortfelName.Text);
             Fundusze.Clear();
             NewPortfelName.Clear();
-        }
+            }
 
+        private void btUsuń_Click(object sender, RoutedEventArgs e)
+        {
+               
+        }
     }
 }
