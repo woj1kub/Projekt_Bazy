f704eb9abd740308de4a6e505e9b1c3f28672b10
dbarczak <92883028+dbarczak@users.noreply.github.com>
2023-06-30 18:37:34 +0200

    Raport, historia, lokaty coś tam(nie działaja)

--- Bazy/GlownePanele/HistoriePanel.xaml.cs
+++ Bazy/GlownePanele/HistoriePanel.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,37 @@ namespace Bazy
         public HistoriePanel()
         {
             InitializeComponent();
+        }
+
+
+        public class Historia
+        {
+            public string Opis { get; set; }
+            public decimal Kwota { get; set; }
+            public DateTime Data { get; set; }
+        }
+
+
+
+        public void UzupelnijHistorie()
+        {
+            var historia = new ObservableCollection<Historia>();
+
+            using (var conn = new NpgsqlConnection(Registration.ConnString()))
+            {
+                conn.Open();
+                var cmd = new NpgsqlCommand("SELECT \"Opis_Transakcji\", \"Kwota\", \"Data_Transakcji\" FROM \"Historia Transakcji Portfelu\" ORDER BY \"Data_Transakcji\" DESC", conn);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        historia.Add(new Historia { Opis = reader.GetString(0), Kwota = reader.GetDecimal(1), Data = reader.GetDateTime(2) });
+                    }
+                }
+            }
+
+            ltvHistoria.ItemsSource = historia;
         }
     }
 }
